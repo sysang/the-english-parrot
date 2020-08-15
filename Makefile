9e6658b966385e0ee19a9508f9ca6d71fd9dbe41
sysang <sysangtiger@gmail.com>
2020-08-15 22:51:50 +0700

    new progress

--- Makefile
+++ Makefile
@@ -16,6 +16,16 @@ help:
 action:
 	bin/rasa run actions --actions actions
 
+actiond:
+	setsid ./runactionserver.sh >./actions/actions.log 2>&1 < /dev/null &
+
+stopactiond:
+	ps -ef | awk '/[r]unactionserver\.sh/{print $$2;}' | xargs echo "-${1}" | sed 's/\s//g' | xargs kill -TERM
+
+restartactiond:
+	ps -ef | awk '/[r]unactionserver\.sh/{print $$2;}' | xargs echo "-${1}" | sed 's/\s//g' | xargs kill -TERM
+	setsid ./runactionserver.sh >./actions/actions.log 2>&1 < /dev/null &
+
 training:
 	bin/rasa train -vv
 
@@ -23,7 +33,8 @@ shell:
 	bin/rasa shell -vv -m $(model)
 
 run:
-	bin/rasa run -vv -m $(model)
+	@export ts=`/bin/date "+%Y%m%d.%H%M"`
+	bin/rasa run -vv -m $(model) --log-file=$(model)_run_$(ts).log
 
 formatter:
 	black actions
@@ -34,3 +45,7 @@ lint:
 
 types:
 	pytype --keep-going actions
+
+timestamp:
+	@export ts=`/bin/date "+%Y%m%d.%H%M"`
+	@echo $(ts)
--- e2etest/test.py
+++ e2etest/test.py
@@ -2,22 +2,52 @@ from ruamel.yaml import YAML
 import requests
 import json
 import logging
+import argparse
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', required=True)
+parser.add_argument('--scriptfile', required=True)
+args = parser.parse_args()
+
+model = args.model
+model = model.replace('models/', '')
+model = model.replace('.tar.gz', '')
+
+scriptfile = args.scriptfile
+
+with open(scriptfile, 'r') as f:
+    scripts = f.read()
+
+scriptfile = scriptfile.replace('e2etest/data/','')
+scriptfile = scriptfile.replace('.yml','')
+
+now = datetime.now()
+resultfile = f"e2etest/results/{model}_{scriptfile}_{now.hour}{now.minute}.txt"
 
 logger = logging.getLogger(__name__)
-
-with open('e2etest/data/test_main_scenario.yml', 'r') as f:
-    data = f.read()
+hdlr = logging.FileHandler(resultfile)
+formatter = logging.Formatter('%(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
 
 yaml=YAML(typ="safe", pure=True)
-data = yaml.load(data)
-inputs = data['inputs']
+scripts = yaml.load(scripts)
+inputs = scripts['inputs']
 
+apiurl = 'http://127.0.0.1:5005/webhooks/rest/webhook'
 for msg in inputs:
     payload = { 'sender': 'rasa', 'message': msg }
-    r = requests.post('http://127.0.0.1:5005/webhooks/rest/webhook', data = json.dumps(payload))
+    r = requests.post(apiurl, data = json.dumps(payload))
     botres = [item['text'] for item in r.json()]
     botres = ''.join(botres)
+
     print(f"user >  {msg}")
     print("\n")
     print(f"bot  :  {botres}")
+
+    logger.info(f"user >  {msg}")
+    logger.info("\n")
+    logger.info(f"bot  :  {botres}")
 
