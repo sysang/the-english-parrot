43d7f4c445e8e79f0eec70cd4684b346d2f41159
sysang <daosysang@gmail.com>
2021-09-04 16:49:02 +0700

    Significant improvement in performance

--- modules/rasa-slot-types/RasaSlotTypes/__init__.py
+++ modules/rasa-slot-types/RasaSlotTypes/__init__.py
@@ -1,14 +1,13 @@
 from .binary_vector import OnehotVector99, Binary127Bit3
 from .sentence_transformer import EmbeddedSentence
 from .doc2vec import WordVector
-from .influential_feature import Influentialx4, Influentialx8, Influentialx16, Influentialx10
+from .influential_feature import Influentialx4, Influentialx8, Influentialx16, Influentialx32
 
 __all__ = [
     "OnehotVector99",
     "EmbeddedSentence",
     "Binary127Bit3",
     "WordVector",
-    "Influentialx10",
     "Influentialx4",
     "Influentialx8",
     "Influentialx16",
